22c220250ff601e3a2569be2bd2d465be4888b68
luudvig <ludvigolsson@outlook.com>
2022-11-15 20:33:05 +0100

    created second site and update script

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+set -e
+
+if [ ${#} -eq 0 ]
+then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+/usr/bin/docker compose --file ${1}/docker-compose.yml pull
+/usr/bin/docker compose --file ${1}/docker-compose.yml down
+
+/usr/bin/tar --create --directory=${1} --file=${1}/data-site-a.tgz --gzip data-site-a
+/usr/bin/tar --create --directory=${1} --file=${1}/data-site-b.tgz --gzip data-site-b
+/usr/bin/tar --create --directory=${1} --file=${1}/data-db.tgz --gzip data-db
+
+/usr/bin/docker compose --file ${1}/docker-compose.yml up --detach
+/usr/bin/docker image prune --all --force > $(/usr/bin/mktemp)
