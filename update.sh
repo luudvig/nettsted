07c01e6775534070791957393c681c758e86aa01
luudvig <ludvigolsson@outlook.com>
2022-11-23 18:47:34 +0000

    made possible to include drop-in update scripts

--- update.sh
+++ update.sh
@@ -15,5 +15,10 @@ fi
 /usr/bin/tar --create --directory=${1} --file=${1}/data-site-b.tgz --gzip data-site-b
 /usr/bin/tar --create --directory=${1} --file=${1}/data-db.tgz --gzip data-db
 
+for executable in $(/usr/bin/find ${1}/update.d -executable -type f)
+do
+    ${executable}
+done
+
 /usr/bin/docker compose --file ${1}/docker-compose.yml up --detach
 /usr/bin/docker image prune --all --force > $(/usr/bin/mktemp)
